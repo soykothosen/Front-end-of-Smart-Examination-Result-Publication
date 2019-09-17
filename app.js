74b6e82437131064287670aa49abfee565df807e
Soykot Hosen <soykothosen1603028@gmail.com>
2019-09-17 23:35:44 +0600

    Sign Up page is done

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/',function(req,res){
 	res.render('login',{});
 });
 
+app.get('/signup',function(req,res){
+	res.render('signup',{});
+});
+
 
 app.listen('8080', function(err){
 	if(err){
